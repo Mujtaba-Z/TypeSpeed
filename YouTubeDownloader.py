043ea09df7c0c67ce15b6327cd7c7847fc693290
Mujtaba Zaidi <mujtabazaidi2003@gmail.com>
2022-08-18 13:15:34 -0400

    Imported libraries and set up display
    Co-authored-by: NinjaCentury <NinjaCentury@users.noreply.github.com>

---
+++ YouTubeDownloader.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from pytube import Youtube
+
+display = Tk()
+display.geometry('500 X 300')
+display.resizable(0, 0)
+display.title('YouTube Downloader')
+
+display.mainloop()
